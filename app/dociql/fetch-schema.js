121c87bce04d160b5e2d95578e1e6b3f8f23b329
Darko <darko@agitron.io>
2020-11-16 10:51:43 +0100

    custom token changes

--- app/dociql/fetch-schema.js
+++ app/dociql/fetch-schema.js
@@ -1,25 +1,33 @@
-const graphql = require('graphql')
-const request = require("sync-request")
+const graphql = require("graphql");
+const request = require("sync-request");
 
-const converter = require('graphql-2-json-schema');
+const converter = require("graphql-2-json-schema");
 
-module.exports = function (graphUrl) {
+module.exports = function (graphUrl, graphToken, graphRefToken) {
+	const requestBody = {
+		query: graphql.introspectionQuery,
+	};
 
-    const requestBody = {
-        query: graphql.introspectionQuery
-    };
+	const responseBody = request("POST", graphUrl, {
+		json: requestBody,
+		headers: {
+			"x-connector-token": graphToken,
+			"x-connector-refresh-token": graphRefToken,
+			"x-connector-auth-request-type": "LOCAL_STORAGE",
+		},
+	}).getBody("utf8");
 
-    const responseBody = request("POST", graphUrl, {
-        json: requestBody
-    }).getBody('utf8');
+	const introspectionResponse = JSON.parse(responseBody);
 
-    const introspectionResponse = JSON.parse(responseBody);    
+	const jsonSchema = converter.fromIntrospectionQuery(
+		introspectionResponse.data
+	);
+	const graphQLSchema = graphql.buildClientSchema(introspectionResponse.data, {
+		assumeValid: true,
+	});
 
-    const jsonSchema = converter.fromIntrospectionQuery(introspectionResponse.data);
-    const graphQLSchema = graphql.buildClientSchema(introspectionResponse.data, { assumeValid: true});
-
-    return {
-        jsonSchema,
-        graphQLSchema
-    }
-}
+	return {
+		jsonSchema,
+		graphQLSchema,
+	};
+};
